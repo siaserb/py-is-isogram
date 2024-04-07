dd102e54c32ea249695738b4fb10d4e5f24e5357
siaserb <assiya.serb@gmail.com>
2024-04-07 22:48:10 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,20 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word,result",
+    [
+        pytest.param("atmospheric", True, id="testing an isogram word"),
+        pytest.param("smalL", False, id="testing when word has the same "
+                                        "letter in different case"),
+        pytest.param("", True, id="testing empty string"),
+        pytest.param("lOOk", False, id="testing word with two "
+                                       "letters in uppercase"),
+        pytest.param("Arab", False, id="testing word with two "
+                                       "letters in uppercase")
+    ]
+)
+def test_is_isogram(word: str, result: bool) -> None:
+    assert is_isogram(word) == result
